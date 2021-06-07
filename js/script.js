f4d8bc64f9697fec454dfd0ed5fbb0a3a8c5744b
FredHorizon <fredoliveira.sys@gmail.com>
2021-06-07 19:23:52 -0400

    Configura a movimentação dos helicópteros

--- js/script.js
+++ js/script.js
@@ -8,12 +8,31 @@ function start() {
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
   var jogo = {}
+  var velocidade = 5;
+  var posicaoY = parseInt(Math.random() * 334);
+  var TECLA = {
+    W: 87,
+    S: 83,
+    D: 68
+  }
+
+  jogo.pressionou = [];
+
+  $(document).keydown(function (e) {
+    jogo.pressionou[e.which] = true;
+  });
+
+  $(document).keyup(function (e) {
+    jogo.pressionou[e.which] = false;
+  });
 
   jogo.timer = setInterval(loop, 30);
 
   function loop() {
 
     movefundo();
+    movejogador();
+    moveinimigo1();
 
   }
 
@@ -22,6 +41,50 @@ function start() {
     esquerda = parseInt($("#fundoGame").css("background-position"));
     $("#fundoGame").css("background-position", esquerda - 1);
 
+  }
+
+  function movejogador() {
+
+    if (jogo.pressionou[TECLA.W]) {
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo - 10);
+
+      if (topo <= 0) {
+        $("#jogador").css("top", topo + 10);
+      }
+
+    }
+
+    if (jogo.pressionou[TECLA.S]) {
+
+      var topo = parseInt($("#jogador").css("top"));
+      $("#jogador").css("top", topo + 10);
+
+      if (topo >= 434) {
+        $("#jogador").css("top", topo - 10);
+      }
+
+    }
+
+    if (jogo.pressionou[TECLA.D]) {
+
+      //Chama função Disparo
+    }
+
+  }
+
+  function moveinimigo1() {
+
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left", posicaoX - velocidade);
+    $("#inimigo1").css("top", posicaoY);
+
+    if (posicaoX <= 0) {
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+
+    }
   }
 
 }
