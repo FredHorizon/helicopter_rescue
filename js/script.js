52067052eb87d708b78d048cff2f0c4e4f3ae5c1
FredHorizon <fredoliveira.sys@gmail.com>
2021-06-08 18:37:47 -0400

    Configura o disparo do helicóptero

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ function start() {
   $("#fundoGame").append("<div id='inimigo2'></div>");
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+  var podeAtirar = true;
   var jogo = {}
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
@@ -69,8 +70,7 @@ function start() {
     }
 
     if (jogo.pressionou[TECLA.D]) {
-
-      //Chama função Disparo
+      disparo();
     }
 
   }
@@ -111,6 +111,39 @@ function start() {
 
     }
 
+  }
+
+  function disparo() {
+
+    if (podeAtirar == true) {
+
+      podeAtirar = false;
+
+      topo = parseInt($("#jogador").css("top"))
+      posicaoX = parseInt($("#jogador").css("left"))
+      tiroX = posicaoX + 190;
+      topoTiro = topo + 37;
+      $("#fundoGame").append("<div id='disparo'></div");
+      $("#disparo").css("top", topoTiro);
+      $("#disparo").css("left", tiroX);
+
+      var tempoDisparo = window.setInterval(executaDisparo, 30);
+
+    }
+
+    function executaDisparo() {
+      posicaoX = parseInt($("#disparo").css("left"));
+      $("#disparo").css("left", posicaoX + 20);
+
+      if (posicaoX > 900) {
+
+        window.clearInterval(tempoDisparo);
+        tempoDisparo = null;
+        $("#disparo").remove();
+        podeAtirar = true;
+
+      }
+    }
   }
 
 }
