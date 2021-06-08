30d3f72a87a115e36d9a5bd068ee7a6867b94761
FredHorizon <fredoliveira.sys@gmail.com>
2021-06-08 18:26:54 -0400

    Configura a movimentação do caminhão e do boneco

--- js/script.js
+++ js/script.js
@@ -33,6 +33,8 @@ function start() {
     movefundo();
     movejogador();
     moveinimigo1();
+    moveinimigo2();
+    moveamigo();
 
   }
 
@@ -85,6 +87,30 @@ function start() {
       $("#inimigo1").css("top", posicaoY);
 
     }
+  }
+
+  function moveinimigo2() {
+    posicaoX = parseInt($("#inimigo2").css("left"));
+    $("#inimigo2").css("left", posicaoX - 3);
+
+    if (posicaoX <= 0) {
+
+      $("#inimigo2").css("left", 775);
+
+    }
+  }
+
+  function moveamigo() {
+
+    posicaoX = parseInt($("#amigo").css("left"));
+    $("#amigo").css("left", posicaoX + 1);
+
+    if (posicaoX > 906) {
+
+      $("#amigo").css("left", 0);
+
+    }
+
   }
 
 }
