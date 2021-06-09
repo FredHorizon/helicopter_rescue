12cbb50fe55d5985f076c6a7aee91dd01fa00a09
FredHorizon <fredoliveira.sys@gmail.com>
2021-06-08 20:13:47 -0400

    Finaliza as configurações de colisões e animações

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function start() {
   $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
   var podeAtirar = true;
+  var fimdejogo = false;
   var jogo = {}
   var velocidade = 5;
   var posicaoY = parseInt(Math.random() * 334);
@@ -149,6 +150,11 @@ function start() {
 
   function colisao() {
     var colisao1 = ($("#jogador").collision($("#inimigo1")));
+    var colisao2 = ($("#jogador").collision($("#inimigo2")));
+    var colisao3 = ($("#disparo").collision($("#inimigo1")));
+    var colisao4 = ($("#disparo").collision($("#inimigo2")));
+    var colisao5 = ($("#jogador").collision($("#amigo")));
+    var colisao6 = ($("#inimigo2").collision($("#amigo")));
 
     if (colisao1.length > 0) {
 
@@ -159,6 +165,62 @@ function start() {
       posicaoY = parseInt(Math.random() * 334);
       $("#inimigo1").css("left", 694);
       $("#inimigo1").css("top", posicaoY);
+    }
+
+    if (colisao2.length > 0) {
+
+      inimigo2X = parseInt($("#inimigo2").css("left"));
+      inimigo2Y = parseInt($("#inimigo2").css("top"));
+      explosao2(inimigo2X, inimigo2Y);
+
+      $("#inimigo2").remove();
+
+      reposicionaInimigo2();
+
+    }
+
+    if (colisao3.length > 0) {
+
+      inimigo1X = parseInt($("#inimigo1").css("left"));
+      inimigo1Y = parseInt($("#inimigo1").css("top"));
+
+      explosao1(inimigo1X, inimigo1Y);
+      $("#disparo").css("left", 950);
+
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+
+    }
+
+    if (colisao4.length > 0) {
+
+      inimigo2X = parseInt($("#inimigo2").css("left"));
+      inimigo2Y = parseInt($("#inimigo2").css("top"));
+      $("#inimigo2").remove();
+
+      explosao2(inimigo2X, inimigo2Y);
+      $("#disparo").css("left", 950);
+
+      reposicionaInimigo2();
+
+    }
+
+    if (colisao5.length > 0) {
+
+      reposicionaAmigo();
+      $("#amigo").remove();
+    }
+
+    if (colisao6.length > 0) {
+
+      amigoX = parseInt($("#amigo").css("left"));
+      amigoY = parseInt($("#amigo").css("top"));
+      explosao3(amigoX, amigoY);
+      $("#amigo").remove();
+
+      reposicionaAmigo();
+
     }
 
   }
@@ -178,6 +240,75 @@ function start() {
       div.remove();
       window.clearInterval(tempoExplosao);
       tempoExplosao = null;
+
+    }
+
+  }
+
+  function reposicionaInimigo2() {
+
+    var tempoColisao4 = window.setInterval(reposiciona4, 5000);
+
+    function reposiciona4() {
+      window.clearInterval(tempoColisao4);
+      tempoColisao4 = null;
+
+      if (fimdejogo == false) {
+
+        $("#fundoGame").append("<div id=inimigo2></div");
+
+      }
+    }
+  }
+
+  function explosao2(inimigo2X, inimigo2Y) {
+
+    $("#fundoGame").append("<div id='explosao2'></div");
+    $("#explosao2").css("background-image", "url(imgs/explosao.png)");
+    var div2 = $("#explosao2");
+    div2.css("top", inimigo2Y);
+    div2.css("left", inimigo2X);
+    div2.animate({ width: 200, opacity: 0 }, "slow");
+
+    var tempoExplosao2 = window.setInterval(removeExplosao2, 1000);
+
+    function removeExplosao2() {
+
+      div2.remove();
+      window.clearInterval(tempoExplosao2);
+      tempoExplosao2 = null;
+
+    }
+
+  }
+
+  function reposicionaAmigo() {
+
+    var tempoAmigo = window.setInterval(reposiciona6, 6000);
+
+    function reposiciona6() {
+      window.clearInterval(tempoAmigo);
+      tempoAmigo = null;
+
+      if (fimdejogo == false) {
+
+        $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+      }
+
+    }
+
+  }
+
+  function explosao3(amigoX, amigoY) {
+    $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+    $("#explosao3").css("top", amigoY);
+    $("#explosao3").css("left", amigoX);
+    var tempoExplosao3 = window.setInterval(resetaExplosao3, 1000);
+    function resetaExplosao3() {
+      $("#explosao3").remove();
+      window.clearInterval(tempoExplosao3);
+      tempoExplosao3 = null;
 
     }
 
